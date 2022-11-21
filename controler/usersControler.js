bab531bc5f3df45b4256ad4337af9687414fb745
tanvir <shovontanvir124@gmail.com>
2022-11-21 09:08:10 +0600

    WIP:removed duplicate email check and changed created status code from 200 to 201 from create api

--- controler/usersControler.js
+++ controler/usersControler.js
@@ -5,19 +5,9 @@ const getUsers = (req, res, next) => {
 
 const createUser = async (req, res, next) => {
   try {
-    const peopleInfo = await peopleModel.find();
-    peopleInfo.map((data) => {
-      if (data.email == req.body.email) {
-        console.log("The user is already exist!");
-        res.status(409).json({
-          status: 409,
-          message: "The user is already exist!",
-        });
-      }
-    });
     await peopleModel.create(req.body);
-    res.status(200).json({
-      status: 200,
+    res.status(201).json({
+      status: 201,
       message: "User is created successfully",
     });
   } catch (err) {
