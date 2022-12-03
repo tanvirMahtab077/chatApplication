4c69fc762d6189f960dad14dc8258799a49d5148
tanvir <shovontanvir124@gmail.com>
2022-12-03 15:59:02 +0600

    WIP:added user validation api

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const upload = require('../middleware/uploadHandler/uploadFile')
+const {userValidation,addUserValidationHandler} = require('../middleware/addUserValidation')
 
-const {getUsers,createUser} = require('../controler/usersControler')
+const {getUsers,createUser} = require('../controler/usersControler');
+
 
 
 /* GET users listing. */
 router.get('/', getUsers);
-router.post('/create',upload.single('avatar'),createUser)
+router.post('/create',upload.single('avatar'),userValidation,addUserValidationHandler,createUser)
 
 module.exports = router;
